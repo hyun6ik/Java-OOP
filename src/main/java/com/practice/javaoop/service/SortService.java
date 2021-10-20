4a0f3ae17fe83e3a6ed7d95e19a0d02ac3b4fef8
hyun6ik <fkanwm@naver.com>
2021-10-20 19:40:54 +0900

    feat(#5) : 스프링 의존성 추가

--- src/main/java/com/practice/javaoop/service/SortService.java
+++ src/main/java/com/practice/javaoop/service/SortService.java
@@ -1,15 +1,17 @@
 package com.practice.javaoop.service;
 
-import com.practice.javaoop.logic.JavaSort;
 import com.practice.javaoop.logic.Sort;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class SortService {
 
     private final Sort<String> sort;
 
-    public SortService(Sort<String> sort) {
+    public SortService(@Qualifier("bubbleSort") Sort<String> sort) {
         this.sort = sort;
     }
 
