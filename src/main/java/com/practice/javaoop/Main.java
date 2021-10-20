1a844aee56cacfe7417c51ea04508ec585fb5bb0
hyun6ik <fkanwm@naver.com>
2021-10-20 19:05:29 +0900

    #3 - 정렬 인터페이스 설계 및 각 구현체 오버라이딩

--- src/main/java/com/practice/javaoop/Main.java
+++ src/main/java/com/practice/javaoop/Main.java
@@ -1,13 +1,14 @@
 package com.practice.javaoop;
 
 import com.practice.javaoop.logic.BubbleSort;
+import com.practice.javaoop.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        final BubbleSort<String> sort = new BubbleSort<>();
+        final Sort<String> sort = new BubbleSort<>();
 
         System.out.println("[result] " + sort.sort(Arrays.asList(args)));
     }
